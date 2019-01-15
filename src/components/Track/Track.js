4d9a59ecf6b231b3d323a305eb28b953d5a19d11
Paz <pezet.pascal@gmail.com>
2019-01-15 11:34:36 +1100

    Added searchBar code, SearchResult code, TrackList and Track. All static for now

---
+++ src/components/Track/Track.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+    render () {
+        return(
+            <div class="Track">
+                <div class="Track-information">
+                    <h3>Tiny Dancer</h3>
+                    <p>Elton John | Madman Across The Water</p>
+                </div>
+                <a class="Track-action">+</a>
+            </div>
+        );
+    }
+}
+
+export default Track;
