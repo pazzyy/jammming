bb1264f1fdcc4c380271fae9637ebd66fdfd530b
Migrated - Pascal Pezet <pascal.pezet@PascalPezet-MBP.local>
2019-01-18 14:30:53 +1100

    fixed the map undefined by pointing url to the authorized endpoint

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -6,11 +6,10 @@ const Spotify = {
     getAccessToken() {
         if(accessToken){
             return accessToken;
-        } 
-        else if(window.location.href.match(/access_token=([^&]*)/) && window.location.href.match(/expires_in=([^&]*)/))
+        } else if(window.location.href.match(/access_token=([^&]*)/) && window.location.href.match(/expires_in=([^&]*)/))
         {
           accessToken = window.location.href.match(/access_token=([^&]*)/)[1];
-          let expiresIn = window.location.href.match(/expires_in=([^&]*)/)[1];
+          let   expiresIn = window.location.href.match(/expires_in=([^&]*)/)[1];
       
           window.setTimeout(() => accessToken = '', expiresIn*1000);
           window.history.pushState('Access Token', null, '/');
@@ -19,13 +18,14 @@ const Spotify = {
         }
         else
         {
-          let url = `https://api.spotify.com/v1/search?type=track&q=TERM
-          `;
+          let url = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
           window.location = url;
         }
       },
     
     search(term) {
+        let accessToken = this.getAccessToken();
+
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
